2d1e2d541c378e2e92aed52925cd264675f50f4d
Anmol Sharma <demondevil630@gmail.com>
2024-07-21 19:49:43 -0400

    New Code

--- next-level-deals-frontend/src/pages/Search.jsx
+++ next-level-deals-frontend/src/pages/Search.jsx
@@ -1,12 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import {Link} from 'react-router-dom'
-import Navbar from '../components/navbar/Navbar'
-import brandLogo from '../assets/nld_logo.png'
-import '../App.css'
 
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from '../components/navbar/Navbar';
+import brandLogo from '../assets/nld_logo.png';
+import '../App.css';
 
 function Search() {
-
   const [allDeals, setAllDeals] = useState([]);
   const [filteredDeals, setFilteredDeals] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
@@ -19,24 +18,23 @@ function Search() {
     setAllDeals(resultsJson);
     setFilteredDeals(resultsJson);
     console.log(resultsJson);
-  }
+  };
 
   useEffect(() => {
     getAllGames(pageNum);
   }, [pageNum]);
 
-  const filterDeals = (minPrice, maxPrice) => {
-    const filtered = allDeals.filter(
-      (game) => game.salePrice >= minPrice && game.salePrice <= maxPrice
-    );
-    setFilteredDeals(filtered);
+  const fetchFilteredDeals = async (endpoint) => {
+    const results = await fetch(`http://localhost:3000${endpoint}`);
+    const resultsJson = await results.json();
+    setFilteredDeals(resultsJson);
+    console.log(resultsJson);
   };
+
   const showAllDeals = () => {
     setFilteredDeals(allDeals);
   };
-  const sortDealsByPrice = (deals) => {
-    return deals.sort((a, b) => a.salePrice - b.salePrice);
-  };
+
   const handleSearch = (event) => {
     const query = event.target.value.toLowerCase();
     setSearchQuery(query);
@@ -47,15 +45,15 @@ function Search() {
   };
 
   const handleNext = () => {
-    setPageNum(prevPageNum => prevPageNum + 1);
-  }
+    setPageNum((prevPageNum) => prevPageNum + 1);
+  };
 
   const handlePrev = () => {
-    if (pageNum == 0) {
+    if (pageNum === 0) {
       return;
     }
-    setPageNum(prevPageNum => prevPageNum - 1);
-  }
+    setPageNum((prevPageNum) => prevPageNum - 1);
+  };
 
   console.log(pageNum);
 
@@ -69,11 +67,41 @@ function Search() {
       <h1 className='mt-5 text-light'> Search Games</h1>
 
       <div className="row justify-content-center mt-4">
-        <button type='button' className='btn btn-dark col-2 me-1' onClick={() => filterDeals(0, 4.99)}> $0 - $4.99   </button>
-        <button type='button' className='btn btn-dark col-2 me-1' onClick={() => filterDeals(5, 9.99)}> $5 - $9.99   </button>
-        <button type='button' className='btn btn-dark col-2 me-1' onClick={() => filterDeals(10, 24.99)}> $10 - $24.99 </button>
-        <button type='button' className='btn btn-dark col-2 me-1' onClick={() => filterDeals(25, Infinity)}> $25+         </button>
-        <button type='button' className='btn btn-dark col-2 me-1' onClick={showAllDeals}> All Games </button>
+        <button
+          type='button'
+          className='btn btn-dark col-2 me-1'
+          onClick={() => fetchFilteredDeals('/dealsUnder5Dollars')}
+        >
+          $0 - $4.99
+        </button>
+        <button
+          type='button'
+          className='btn btn-dark col-2 me-1'
+          onClick={() => fetchFilteredDeals('/dealsUnder10Dollars')}
+        >
+          $5 - $9.99
+        </button>
+        <button
+          type='button'
+          className='btn btn-dark col-2 me-1'
+          onClick={() => fetchFilteredDeals('/dealsUnder25Dollars')}
+        >
+          $10 - $24.99
+        </button>
+        <button
+          type='button'
+          className='btn btn-dark col-2 me-1'
+          onClick={() => fetchFilteredDeals('/dealsAbove25Dollars')}
+        >
+          $25+
+        </button>
+        <button
+          type='button'
+          className='btn btn-dark col-2 me-1'
+          onClick={showAllDeals}
+        >
+          All Games
+        </button>
       </div>
 
       <div className="row justify-content-center mt-4">
@@ -83,12 +111,10 @@ function Search() {
           placeholder="Search by game title"
           value={searchQuery}
           onChange={handleSearch}
-
         />
       </div>
 
       <table className="table table-dark table-hover mt-5">
-
         <thead>
           <tr>
             <th scope="col">Normal Price</th>
@@ -97,14 +123,13 @@ function Search() {
             <th scope="col">Steam Rating</th>
           </tr>
         </thead>
-
         <tbody>
-          {sortDealsByPrice(filteredDeals).map((game, index) => (
+          {filteredDeals.map((game, index) => (
             <tr key={index}>
               <td>{game.normalPrice}</td>
               <td>{game.salePrice}</td>
               <td>
-              <Link to={`https://www.cheapshark.com/redirect?dealID=${game.dealID}`} className='text-light'>
+                <Link to={`https://www.cheapshark.com/redirect?dealID=${game.dealID}`} className='text-light'>
                   {game.title}
                 </Link>
               </td>
@@ -112,7 +137,6 @@ function Search() {
             </tr>
           ))}
         </tbody>
-
       </table>
 
       <nav aria-label="Page navigation">
@@ -121,12 +145,12 @@ function Search() {
             <Link className={"page-link" + (pageNum === 0 ? " disabled" : "")} onClick={handlePrev}>Prev</Link>
           </li>
           <li className="page-item">
-            <Link className={"page-link" + (pageNum === 50 ? " disabled" : "")} onClick={handleNext}>Next</Link>
+            <Link className={"page-link"} onClick={handleNext}>Next</Link>
           </li>
         </ul>
       </nav>
     </>
-  )
+  );
 }
 
-export default Search
+export default Search;
--- routes/index.js
+++ routes/index.js
@@ -6,44 +6,53 @@ router.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-//Deals under $5
+// Deals under $5
 router.get('/allDeals/:pageNumber', async (req, res) => {
   try {
-      const pageNum = req.params.pageNumber;
-      console.log(req.params.pageNumber);
-      const response = await axios.get(`https://www.cheapshark.com/api/1.0/deals?pageNumber=${pageNum}&pageSize=30`);
-      // console.log(response.data); // Log the data to the console
-      // const totalPages = response.headers['x-total-page-count']
-      res.status(200).json(response.data); // Send the data back as a JSON response
+    const pageNum = req.params.pageNumber;
+    console.log(req.params.pageNumber);
+    const response = await axios.get(`https://www.cheapshark.com/api/1.0/deals?pageNumber=${pageNum}&pageSize=30&sortBy=price&desc=0&storeID=1`);
+    res.status(200).json(response.data); // Send the data back as a JSON response
   } catch (error) {
-      console.error('Error fetching data from API:', error);
-      res.status(500).send('An error occurred while fetching data.');
+    console.error('Error fetching data from API:', error);
+    res.status(500).send('An error occurred while fetching data.');
+  }
+});
+
+router.get('/allDeals', async (req, res) => {
+  try {
+    const { pageNumber, pageSize, sortBy } = req.query;
+    const response = await axios.get(`https://www.cheapshark.com/api/1.0/deals?pageNumber=${pageNumber}&pageSize=${pageSize}&sortBy=${sortBy}`);
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error('Error fetching data from API:', error);
+    res.status(500).send('An error occurred while fetching data.');
   }
 });
 
 
-//Deals under $5
 router.get('/dealsUnder5Dollars', async (req, res) => {
   try {
-      const response = await axios.get('https://www.cheapshark.com/api/1.0/deals?upperPrice=5');
-      console.log(response.data); // Log the data to the console
-      res.status(200).json(response.data); // Send the data back as a JSON response
+    const response = await axios.get('https://www.cheapshark.com/api/1.0/deals?sortBy=price&desc=0&upperPrice=5');
+    console.log(response.data); // Log the data to the console
+    res.status(200).json(response.data); // Send the data back as a JSON response
   } catch (error) {
-      console.error('Error fetching data from API:', error);
-      res.status(500).send('An error occurred while fetching data.');
+    console.error('Error fetching data from API:', error);
+    res.status(500).send('An error occurred while fetching data.');
   }
 });
+
 
 
 //Deals under $10 and above $5
 router.get('/dealsUnder10Dollars', async (req, res) => {
   try {
-      const response = await axios.get('https://www.cheapshark.com/api/1.0/deals?lowerPrice=5&upperPrice=10');
-      console.log(response.data); // Log the data to the console
-      res.status(200).json(response.data); // Send the data back as a JSON response
+    const response = await axios.get('https://www.cheapshark.com/api/1.0/deals?sortBy=price&desc=0&lowerPrice=5&upperPrice=10');
+    console.log(response.data); // Log the data to the console
+    res.status(200).json(response.data); // Send the data back as a JSON response
   } catch (error) {
-      console.error('Error fetching data from API:', error);
-      res.status(500).send('An error occurred while fetching data.');
+    console.error('Error fetching data from API:', error);
+    res.status(500).send('An error occurred while fetching data.');
   }
 });
 
@@ -51,24 +60,24 @@ router.get('/dealsUnder10Dollars', async (req, res) => {
 //Deals under $25 and above $10
 router.get('/dealsUnder25Dollars', async (req, res) => {
   try {
-      const response = await axios.get('https://www.cheapshark.com/api/1.0/deals?lowerPrice=10&upperPrice=25');
-      console.log(response.data); // Log the data to the console
-      res.status(200).json(response.data); // Send the data back as a JSON response
+    const response = await axios.get('https://www.cheapshark.com/api/1.0/deals?sortBy=price&desc=0&lowerPrice=10&upperPrice=25');
+    console.log(response.data); // Log the data to the console
+    res.status(200).json(response.data); // Send the data back as a JSON response
   } catch (error) {
-      console.error('Error fetching data from API:', error);
-      res.status(500).send('An error occurred while fetching data.');
+    console.error('Error fetching data from API:', error);
+    res.status(500).send('An error occurred while fetching data.');
   }
 });
 
 //Deals above $25
 router.get('/dealsAbove25Dollars', async (req, res) => {
   try {
-      const response = await axios.get('https://www.cheapshark.com/api/1.0/deals?lowerPrice=25');
-      console.log(response.data); // Log the data to the console
-      res.status(200).json(response.data); // Send the data back as a JSON response
+    const response = await axios.get('https://www.cheapshark.com/api/1.0/deals?sortBy=price&desc=0&lowerPrice=25');
+    console.log(response.data); // Log the data to the console
+    res.status(200).json(response.data); // Send the data back as a JSON response
   } catch (error) {
-      console.error('Error fetching data from API:', error);
-      res.status(500).send('An error occurred while fetching data.');
+    console.error('Error fetching data from API:', error);
+    res.status(500).send('An error occurred while fetching data.');
   }
 });
 
