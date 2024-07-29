e6f40363514ca7bb41e22c9037b70370a76a302f
Anmol Sharma <demondevil630@gmail.com>
2024-07-29 19:42:16 -0400

    server side changed

--- server/app.js
+++ server/app.js
@@ -19,6 +19,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors({
   origin: '*'
 }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 // {
 //   origin: function (origin, callback) {
 //     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
